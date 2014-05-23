cc342b5aaee7df1eec68c53f8b3d396becdbdb61
Raul Pelegrini Neto <raulpelegrinineto@gmail.com>
2014-05-23 16:22:22 -0300

    Classe do Bluetooth pronta, falta testar.

--- bluetooth.cpp
+++ bluetooth.cpp
@@ -1,2 +1,27 @@
 #include "bluetooth.h"
 
+int main(){
+    char c;
+    if (btSerial.available()) {
+        char c = btSerial.read();
+        //Serial.print(".");
+        Serial.write(c);
+        //Serial.print(c, HEX);
+        //delay(50);
+        return;
+    }
+
+    if (Serial.available()) {
+        char c = Serial.read();
+        //if (c == '#') {
+            //btSerial.write("\r\n");
+            //Serial.print("#\n");
+    //} else
+        btSerial.write(c);
+        //Serial.print(" ");
+        //Serial.print(c);
+        //Serial.print(c, HEX);
+        //delay(50);
+    return;
+    }
+}
--- bluetooth.h
+++ bluetooth.h
@@ -2,12 +2,83 @@
 #define bluetooth_h
 
 #include "Arduino.h"
+#include "SoftwareSerial"
 
 class Bluetooth{
 private:
-	
+    int rxPin;
+    int txPin;
+    SoftwareSerial btSerial(rxPin,txPin);
 public:
-
+    int getrxPin();
+    void setrxPin(int rx);
+    int gettxPin();
+    void settxPin(int tx);
+    void ConfiguraBluetooth();
+    Bluetooth();
+    Bluetooth(int r, int t);
 };
+
+Bluetooth::getrxPin(){
+    return rxPin;
+}
+
+Bluetooth::setrxPin(int rx){
+    rxPin = rx;
+}
+
+Bluetooth::gettxPin(){
+    return txPin;
+}
+
+Bluetooth::settxPin(int tx){
+    txPin = tx;
+}
+
+Bluetooth::ConfiguraBluetooth(){
+    // Configurando para 9600
+    btSerial.write("AT+BAUD4");
+    delay(1100);
+    while (btSerial.available())
+        Serial.write(btSerial.read());
+
+    // Configurando para NOME = \"unesp-cti\"
+    btSerial.write("AT+NAMEunesp-cti");
+    delay(1100);
+    while (btSerial.available())
+        Serial.write(btSerial.read());
+
+    // Configurando PIN para 9889
+    btSerial.write("AT+PIN9889");
+    delay(1100);
+    while (btSerial.available())
+        Serial.write(btSerial.read());
+
+    // Versao do bluetooth
+    btSerial.write("AT+VERSION");
+    delay(1100);
+    while (btSerial.available())
+        Serial.write(btSerial.read());
+}
+
+Bluetooth::Bluetooth(){
+    setrxPin(2);
+    settxPin(3);
+    Serial.begin(9600);
+    pinMode(txPin, OUTPUT);
+    pinMode(rxPin, INPUT);
+    btSerial.begin(9600);
+    ConfiguraBluetooth();
+}
+
+Bluetooth::Bluetooth(int r, int t){
+    setrxPin(r);
+    settxPin(t);
+    Serial.begin(9600);
+    pinMode(txPin, OUTPUT);
+    pinMode(rxPin, INPUT);
+    btSerial.begin(9600);
+    ConfiguraBluetooth();
+}
 
 #endif
